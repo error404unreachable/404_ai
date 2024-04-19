c0c05aa8b186501213e5cd650557714700ba1e63
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2024-04-19 11:44:04 +0000

    Updated index and js

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 // Created a new Neural Network
 const network = new brain.NeuralNetwork();
+
+// Training Network with 4 input objects
+network.train([
+    {input:[0,0], output:{zero:1}},
+    {input:[0,1], output:{one:1}},
+    {input:[1,0], output:{one:1}},
+    {input:[1,1], output:{zero:1}},
+]);
+
+// Expected output of [1,0]
+result = network.run([1,0]);
+
+// Display probability
+document.getElementById("out").innerText = result["one"] + " " + result["zero"];
