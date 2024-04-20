da21ffbbeef0c55347224ce29d3af8962773315b
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2024-04-20 03:10:25 +0000

    Update

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 // Created a new Neural Network
-const network = new brain.NeuralNetwork({ hiddenLayers: [1] });
+const network = new brain.NeuralNetwork({hiddenLayers: [3]});
 
-const trainingData = [
-    {input: [0,0], output: [0]},
-    {input: [0,1], output: [1]},
-    {input: [1,0], output: [1]},
-    {input: [1,1], output: [0]}
+const colors = [
+    {green: 0.2, blue: 0.4},
+    {green: 0.4, blue: 0.6},
+    {red: 0.2, green: 0.8, blue: 0.8}
 ];
 
-network.train(trainingData, {
-    log: (error) => console.log(error)
-});
+const brightnesses = [
+    {dark: 0.8},
+    {neutral: 0.8},
+    {light: 0.7}
+];
+
+const trainingData = [];
+
+for (let i = 0; i < VideoColorSpace.length; i++) {
+    trainingData.push({
+        input: colors[i],
+        output: brightnesses[i]
+    });
+}
+
+const stats = net.train(trainingData);
+
+console.log(stats)
