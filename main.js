13f71ef620f6bc20097aad9e8f5c85bd175fffaa
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2024-04-20 02:38:16 +0000

    Update js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ network.train([
     {input:[0,0], output:[0]},
     {input:[0,1], output:[1]},
     {input:[1,0], output:[1]},
-    {input:[1,1], output:[0]},
+    {input:[1,1], output:[0]}
 ]);
 
 // Expected output of [0,0]
