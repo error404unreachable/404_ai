bdd244d21df3901ddcc8203be4aa01706c038738
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2024-04-24 10:04:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const network = new brain.NeuralNetwork();
 
 network.train([
-    {input:[0,0], output:[zero:1]},
-    {input:[0,1], output:[one:1]},
-    {input:[1,0], output:[one:1]},
-    {input:[1,1], output:[zero:1]}
+    {input:[0,0], output:{zero:1}},
+    {input:[0,1], output:{one:1}},
+    {input:[1,0], output:{one:1}},
+    {input:[1,1], output:{zero:1}}
 ]);
 
 result = network.run([1,0]);
 
-document.getElementById("out").innerText = result["one"] + " _ " + result["zero"];
+document.getElementById("out").innerText = result["one"] + "  " + result["zero"];
